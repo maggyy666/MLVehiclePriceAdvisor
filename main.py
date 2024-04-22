adbfc4f0abc3a0ccb50a455cef38de77e4e660ee
maggyy666 <kczyz20@gmail.com>
2024-04-22 22:02:06 +0200

    Loading mroe content on site

--- main.py
+++ main.py
@@ -32,12 +32,23 @@ except Exception as e:
 
 wait = WebDriverWait(driver, 50)
 try:
-    for i in range(1, 11):
+    for i in range(1, 30):
         current_city = base_city.format(x=i)
         current_price = base_price.format(x=i)
-        city_name = wait.until(EC.presence_of_element_located((By.XPATH, current_city))).text
-        price_name = wait.until(EC.presence_of_element_located((By.XPATH, current_price))).text
+
+        city_element = wait.until(EC.visibility_of_element_located((By.XPATH, current_city)))
+        price_element = wait.until(EC.visibility_of_element_located((By.XPATH, current_price)))
+
+        city_name = city_element.text if city_element.is_displayed() else "Not Visible"
+        price_name = price_element.text if price_element.is_displayed() else "Not Visible"
+
         print(f"{city_name},{price_name}")
+        if i == 12:
+            load_more_button = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="LWbU-showMoreButton"]/div[1]/div')))
+            load_more_button.click()
+            time.sleep(2)
+
+
 except Exception as e:
         print(f"Error for path {current_city}: {e}")
 
