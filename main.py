cf760531a16e564a4ed27138a3429a3c94bd0790
maggyy666 <kczyz20@gmail.com>
2024-04-25 21:03:56 +0200

    Added try catch blocks

--- main.py
+++ main.py
@@ -19,60 +19,58 @@ driver = webdriver.Chrome()
 
 url = "https://www.otomoto.pl/osobowe"
 try:
-    for i in range(1, 30):
-        current_city = base_city.format(x=i)
-        current_price = base_price.format(x=i)
+    with open(csv_file, 'w',newline='',encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Car_ID. ","ID", "Nazwa pojazdu", "Przebieg", "Cena", "Waluta", "Engine_Power", "On_Page"])
 
-        city_element = wait.until(EC.visibility_of_element_located((By.XPATH, current_city)))
-        price_element = wait.until(EC.visibility_of_element_located((By.XPATH, current_price)))
+        for car_brand in car_brands:
+            current_page = 1
+            car_id = 1
 
-        city_name = city_element.text if city_element.is_displayed() else "Not Visible"
-        price_name = price_element.text if price_element.is_displayed() else "Not Visible"
+            url_count =f"{base_url}{car_brand}"
+            driver.get(url_count)
 
-        print(f"{city_name},{price_name}")
-        if i == 12:
-            load_more_button = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="LWbU-showMoreButton"]/div[1]/div')))
-            load_more_button.click()
-            time.sleep(2)
-
-
-    print(f"Scraping page: {current_page}")
-    articles = driver.find_elements(By.CSS_SELECTOR, 'article[class*="ooa-yca59n e1i3khom0"]')
-    data_ids = []
-    for article in articles:
-        print(f"Car NO. {id}")
-        data_id = article.get_attribute('data-id')
-        data_ids.append(data_id)
-        link_element = article.find_element(By.XPATH, './/a[@target="_self"]')
-        car_name = link_element.text
-        # Mileage
-        dl_mileage_element = article.find_element(By.XPATH, './/dl[@class="ooa-1uwk9ii e1i3khom11"]')
-        mileage_element = dl_mileage_element.find_element(By.XPATH, './/dd[@data-parameter="mileage"]')
-        mileage = mileage_element.text
-        # Type_Of_Fuel
-        dl_fuel_element = article.find_element(By.XPATH, './/dl[@class="ooa-1uwk9ii e1i3khom11"]')
-        fuel_element = dl_fuel_element.find_element(By.XPATH, './/dd[@data-parameter="fuel_type"]')
-        fuel = fuel_element.text
-        # Gearbox
-        dl_gearbox_element = article.find_element(By.XPATH, './/dl[@class="ooa-1uwk9ii e1i3khom11"]')
-        gearbox_element = dl_gearbox_element.find_element(By.XPATH, './/dd[@data-parameter="gearbox"]')
-        gearbox = gearbox_element.text
-        # Year
-        dl_year_element = article.find_element(By.XPATH, './/dl[@class="ooa-1uwk9ii e1i3khom11"]')
-        year_element = dl_year_element.find_element(By.XPATH, './/dd[@data-parameter="year"]')
-        year = year_element.text
-        print("-" * 30)
-        print(f"ID: {data_id}")
-        print(f"Name: {car_name}")
-        print(f"Mileage: {mileage}")
-        print(f"Fuel_Type: {fuel}")
-        print(f"GearBox: {gearbox}")
-        print(f"Year: {year}")
-        print("-" * 30)
-        id += 1
-except Exception as e:
-        print(f"Error for path {current_city}: {e}")
+            try:
+                count_element = driver.find_element(By.XPATH, '//p[@class="e17gkxda2 ooa-17owgto er34gjf0"]/b')
+                count = int(count_element.text.replace(" ",""))
+                print(f"Liczba ogloszen dla: {car_brand.capitalize()}: {count}")
+            except:
+                print(f"Error")
+                continue
+            if count == 0:
+                print(f"Brak ogloszen dla {car_brand.capitalize()}")
+                continue
 
 
+            while car_id <= count and (current_page-1)*32 < count:
+                print(f"Scraping page {current_page} for {car_brand.capitalize()}")
+                url = f"{base_url}{car_brand}?page={current_page}"
+                driver.get(url)
+
+                articles = driver.find_elements(By.CSS_SELECTOR, 'article[class*="ooa-yca59n e1i3khom0"]')
+                data_ids = []
+
+                for article in articles:
+                    print(f"Car NO. {car_id}")
+                    data_id = article.get_attribute('data-id')
+                    data_ids.append(data_id)
+
+                    try:
+                        # Car_Name
+                        link_element = article.find_element(By.XPATH, './/a[@target="_self"]')
+                        car_name = link_element.text
+                    except:
+                        car_name = 'null'
+
+                    try:
+                        # Power
+                        power_element = article.find_element(By.XPATH, './/p[@class="e1i3khom10 ooa-1tku07r er34gjf0"]')
+                        power_text = power_element.text
+                        split = power_text.split("•")
+                        engine_power = split[0].strip()
+                        horse_power = split[1].strip().split("•")[0].strip()
+                    except:
+                        engine_power = 'null'
+                        horse_power = 'null'
 
 driver.quit()
