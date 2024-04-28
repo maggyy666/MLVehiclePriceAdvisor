633c9468f491b91d86808a6d962a2be11a8e9968
maggyy666 <kczyz20@gmail.com>
2024-04-28 16:58:42 +0200

    Created csv_directory for other brands

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from selenium .webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver_path = 'C:\\Users\\kczyz\\PycharmProjects\\ImageDownloader\\chromedriver.exe'
+csv_directory = 'C:\\Users\\kczyz\\PycharmProjects\\KayakWebScraper\\CSV_FILES'
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
