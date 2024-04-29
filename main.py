bab2df0f9f5a28e65e55f659bb5f557d84e9949a
maggyy666 <kczyz20@gmail.com>
2024-04-29 16:37:14 +0200

    Working .py before updates

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 import csv
 import os
 import time
-from selenium .webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver_path = 'C:\\Users\\kczyz\\PycharmProjects\\ImageDownloader\\chromedriver.exe'
 driver_path = 'C:\\Users\\kczyz\\PycharmProjects\\KayakWebScraper\\chromedriver.exe'
 csv_directory = 'C:\\Users\\kczyz\\PycharmProjects\\KayakWebScraper\\CSV_FILES'
 
@@ -16,24 +14,18 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 
 driver = webdriver.Chrome()
-
+car_count = 1
 base_url = "https://www.otomoto.pl/osobowe/"
 limit_pages = 8000
-current_page = 1
-csv_file = 'data.csv'
-status_file = 'status.txt'
-
-
-
-car_brands = ["bmw"]
+csv_data_file = 'data.csv'
 
 car_brands = ["Abarth","Acura","Aiways","Aixam","Alfa-Romeo",
-              "Alpine","Asia","Aston-Martin","Austin",
+              "Alpine","Asia","Aston-Martin","Audi","Austin",
               "Autobianchi","Baic","Bentley","BMW","Alpina",
               "Brilliance","Bugatti","Buick","BYD","Cadillac",
               "Casalini","Caterham","Cenntro","Changan","Chatenet",
               "Chevrolet","Chrysler","Citroen","Cupra","Dacia","Daewoo",
-              "Daihatsu","DeLorean","DFM","DFSK","DKW","Dodge","Doosan",
+              "Daihatsu","DeLorean","DFSK","DKW","Dodge","Doosan",
               "DR-MOTOR","DS-Automobiles","e-go","Elaris","FAW","Ferrari",
               "Fiat","Fisker","Ford","Gaz","Geely","Genesis","GMC","GWM",
               "HiPhi","Honda","Hongqi","Hummer","Hyundai","iamelectric",
@@ -50,46 +42,20 @@ car_brands = ["Abarth","Acura","Aiways","Aixam","Alfa-Romeo",
               "Tata","Tesla","Toyota","Trabant","Triumph","Uaz","Vauxhall","VELEX","Volkswagen",
               "Volvo","Voyah","Warszawa","Wartburg","Wolga","XPeng","Zaporozec","Zastava","ZEEKR","Zuk"]
 
+car_id = 1
+
 try:
-
-    with open(csv_file, 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(
-            ["Car_ID. ", "ID", "Brand", "Model", "Mileage", "Price [PLN]", "Price [EUR]", "Engine_Power",
-             "GearBox", "Year", "Fuel_Type", "Horse_Power", "On_Page"])
-
-    # Otwórz plik data.csv do zapisu danych ogólnych
-    data_csv_exists = os.path.exists(csv_data_file)
-    with open(csv_data_file, 'a' if data_csv_exists else 'w', newline='', encoding='utf-8') as data_file:
+    # Open data.csv for main data
+    with open(csv_data_file, 'a', newline='', encoding='utf-8') as data_file:
         writer_data = csv.writer(data_file)
-        if not data_csv_exists:
-            writer_data.writerow(
-                ["Car_ID", "ID", "Brand", "Model", "Mileage", "Price [PLN]", "Price [EUR]", "Engine_Power",
-                 "GearBox", "Year", "Fuel_Type", "Horse_Power", "On_Page"])
 
-        # Iteracja po każdej marce
         for car_brand in car_brands:
-            # Przechowuje dane tymczasowo w pamięci podręcznej
             brand_data = []
+            brand_csv_file = os.path.join(csv_directory, f'{car_brand}.csv')
 
-            # Sprawdź, czy plik CSV dla danej marki już istnieje
-            if os.path.exists(os.path.join(csv_directory, f'{car_brand}.csv')):
+            if os.path.exists(brand_csv_file):
                 print(f"Plik CSV dla marki {car_brand} już istnieje. Pomijanie...")
                 continue
-
-            # Utwórz nazwę pliku CSV dla danej marki
-            csv_brand_file = os.path.join(csv_directory, f'{car_brand}.csv')
-
-            # Otwórz plik CSV dla danej marki do zapisu danych specyficznych dla marki
-            with open(csv_brand_file, 'w', newline='', encoding='utf-8') as brand_file:
-                writer_brand = csv.writer(brand_file)
-                writer_brand.writerow(
-                    ["Car_ID", "ID", "Brand", "Model", "Mileage", "Price [PLN]", "Price [EUR]", "Engine_Power",
-                     "GearBox", "Year", "Fuel_Type", "Horse_Power", "On_Page"])
-
-            car_id = 1
-
-            fuel_counts = {}
 
             fuel_types = [
                 "hybrid",
@@ -101,13 +67,12 @@ try:
                 "hydrogen",
                 "petrol-cng",
                 "petrol-lpg",
-                "plugin-hybrid",  # Dodanie nowego rodzaju paliwa
+                "plugin-hybrid"
             ]
 
             for fuel_type in fuel_types:
                 url = f"{base_url}{car_brand}?search%5Bfilter_enum_fuel_type%5D={fuel_type}"
                 driver.get(url)
-                #WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, './/h3[@class="e1i3khom16 ooa-1n2paoq er34gjf0"]')))
 
                 try:
                     count_element = driver.find_element(By.XPATH, '//p[@class="e17gkxda2 ooa-17owgto er34gjf0"]/b')
@@ -115,69 +80,38 @@ try:
                 except:
                     count = 0
 
-                fuel_counts[fuel_type] = count
                 print(f"Liczba ogłoszeń dla {fuel_type.capitalize()}: {count} dla {car_brand.capitalize()}")
 
-            groups = [
-                (fuel_counts["hybrid"], "?search%5Bfilter_enum_fuel_type%5D=hybrid"),
-                (fuel_counts["electric"], "?search%5Bfilter_enum_fuel_type%5D=electric"),
-                (fuel_counts["petrol"], "?search%5Bfilter_enum_fuel_type%5D=petrol"),
-                (fuel_counts["diesel"], "?search%5Bfilter_enum_fuel_type%5D=diesel"),
-                (fuel_counts["gas"], "?search%5Bfilter_enum_fuel_type%5D=gas"),
-                (fuel_counts["ethanol"], "?search%5Bfilter_enum_fuel_type%5D=ethanol"),
-                (fuel_counts["hydrogen"], "?search%5Bfilter_enum_fuel_type%5D=hydrogen"),
-                (fuel_counts["petrol-cng"], "?search%5Bfilter_enum_fuel_type%5D=petrol-cng"),
-                (fuel_counts["petrol-lpg"], "?search%5Bfilter_enum_fuel_type%5D=petrol-lpg"),
-                (fuel_counts["plugin-hybrid"], "?search%5Bfilter_enum_fuel_type%5D=plugin-hybrid"),  # Dodanie nowego rodzaju paliwa
-            ]
+                for current_page in range(1, (count // 32) + 2):
+                    print(f"Scraping page {current_page} for {car_brand.capitalize()} and fuel type {fuel_type.capitalize()}")
 
-            for group_count, filter_param in groups:
-                current_page = 1
-                while (current_page - 1) * 32 < group_count:
-                    print(f"Scraping page {current_page} for {car_brand.capitalize()}")
-                    time.sleep(3)
-                    if filter_param:
-                        url = f"{base_url}{car_brand}{filter_param}&page={current_page}"
-                    else:
-                        url = f"{base_url}{car_brand}?page={current_page}"
-
-                    driver.get(url)
-                    WebDriverWait(driver, 10).until(
-                        EC.visibility_of_element_located((By.XPATH, './/h3[@class="e1i3khom16 ooa-1n2paoq er34gjf0"]')))
+                    if current_page > 1:
+                        url = f"{base_url}{car_brand}?search%5Bfilter_enum_fuel_type%5D={fuel_type}&page={current_page}"
+                        driver.get(url)
+                        WebDriverWait(driver, 10).until(
+                            EC.visibility_of_element_located((By.XPATH, './/h3[@class="e1i3khom16 ooa-1n2paoq er34gjf0"]')))
+                        time.sleep(2)
 
                     articles = driver.find_elements(By.CSS_SELECTOR, 'article[class*="ooa-yca59n e1i3khom0"]')
-                    data_ids = []
 
                     for article in articles:
-                        print(f"Car NO. {car_id}")
                         data_id = article.get_attribute('data-id')
-                        data_ids.append(data_id)
-
+                        #Creating an exception for brands that have dual-word names
+                        brand_mapping ={
+                            "Aston": "Aston Martin",
+                            "Alfa": "Alfa Romeo",
+                            "DS": "DS Automobiles",
+                            "Lynk": "Lynk & Co",
+                            "Land": "Land Rover"
+                        }
                         try:
-                            link_element = article.find_element(By.XPATH, './/a[@target="_self"]')
+                            link_element = article.find_element(By.XPATH,'.//a[@target="_self"]')
                             car_name = link_element.text
-                            parts = car_name.split(" ", 1)
-                            if parts[0] == "Aston":
-                                brand_name = "Aston Martin"
-                                car_model = parts[1].replace("Martin", "").strip()
-                            elif parts[0] == "Alfa":
-                                brand_name = "Alfa Romeo"
-                                car_model = parts[1].replace("Romeo", "").strip()
-                            elif parts[0] == "DS":
-                                brand_name = "DS Automobiles"
-                                car_model = parts[1].replace("Automobiles", "").strip()
-                            elif parts[0] == "Lynk":
-                                brand_name = "Lynk & Co"
-                                car_model = parts[1].replace(" & Co", "").strip()
-                            elif parts[0] == "Land":
-                                brand_name = "Land Rover"
-                                car_model = parts[1].replace("Rover", "").strip()
-                            else:
-                                brand_name = parts[0]
-                                car_model = parts[1]
+                            brand_prefix,car_model = car_name.split(" ",1)
+                            brand_name = brand_mapping.get(brand_prefix, brand_prefix)
+                            car_model = car_model.replace(brand_prefix, "").replace("Martin", "").replace("Romeo","").replace("Automobiles", "").replace(" & Co", "").replace("Rover", "").strip()
                         except:
                             car_name = 'null'
-
                         try:
                             power_element = article.find_element(By.XPATH,
                                                                  './/p[@class="e1i3khom10 ooa-1tku07r er34gjf0"]')
@@ -199,24 +133,28 @@ try:
                             mileage = 'null'
 
                         try:
-                            dl_fuel_element = article.find_element(By.XPATH, './/dl[@class="ooa-1uwk9ii e1i3khom11"]')
-                            fuel_element = dl_fuel_element.find_element(By.XPATH, './/dd[@data-parameter="fuel_type"]')
+                            dl_fuel_element = article.find_element(By.XPATH,
+                                                                   './/dl[@class="ooa-1uwk9ii e1i3khom11"]')
+                            fuel_element = dl_fuel_element.find_element(By.XPATH,
+                                                                         './/dd[@data-parameter="fuel_type"]')
                             fuel = fuel_element.text
                         except:
                             fuel = 'null'
 
                         try:
                             dl_gearbox_element = article.find_element(By.XPATH,
-                                                                      './/dl[@class="ooa-1uwk9ii e1i3khom11"]')
+                                                                       './/dl[@class="ooa-1uwk9ii e1i3khom11"]')
                             gearbox_element = dl_gearbox_element.find_element(By.XPATH,
-                                                                              './/dd[@data-parameter="gearbox"]')
+                                                                             './/dd[@data-parameter="gearbox"]')
                             gearbox = gearbox_element.text
                         except:
                             gearbox = 'null'
 
                         try:
-                            dl_year_element = article.find_element(By.XPATH, './/dl[@class="ooa-1uwk9ii e1i3khom11"]')
-                            year_element = dl_year_element.find_element(By.XPATH, './/dd[@data-parameter="year"]')
+                            dl_year_element = article.find_element(By.XPATH,
+                                                                   './/dl[@class="ooa-1uwk9ii e1i3khom11"]')
+                            year_element = dl_year_element.find_element(By.XPATH,
+                                                                         './/dd[@data-parameter="year"]')
                             year = year_element.text
                         except:
                             year = 'null'
@@ -229,7 +167,7 @@ try:
                                                                     './/p[@class="e1i3khom17 ooa-8vn6i7 er34gjf0"]')
                             currency = currency_element.text
 
-                            # Usunięcie spacji i przecinków z ceny, a następnie parsowanie jako float
+                            # Removing spaces/commas and converting to float
                             price = price.replace(' ', '').replace(',', '')
                             price = float(price)
 
@@ -243,8 +181,7 @@ try:
                                 price_pln = 'null'
                                 price_eur = 'null'
 
-
-                            # Formatowanie ceny z separatorem tysięcy
+                            # Formatting price
                             def format_price_with_thousands_separator(price):
                                 parts = str(price).split('.')
                                 integer_part = '{:,.0f}'.format(int(parts[0]))
@@ -252,7 +189,6 @@ try:
                                     return f"{integer_part}.{parts[1]}"
                                 else:
                                     return integer_part
-
 
                             price_pln_formatted = format_price_with_thousands_separator(price_pln)
                             price_eur_formatted = format_price_with_thousands_separator(price_eur)
@@ -262,10 +198,9 @@ try:
                             price_eur = 'null'
                             currency = 'null'
 
-                        writer.writerow(
+                        brand_data.append(
                             [data_id, car_id, brand_name, car_model, mileage, price_pln_formatted, price_eur_formatted,
                              engine_power, gearbox, year, fuel, horse_power, current_page])
-
                         print("-" * 30)
                         print(f"ID: {data_id}")
                         print(f"Brand: {brand_name}")
@@ -278,36 +213,36 @@ try:
                         print(f"Price: {price_pln_formatted} PLN / {price_eur_formatted} EUR")
                         print("-" * 30)
                         car_id += 1
-                    current_page += 1
-                    time.sleep(2)
                         car_count += 1
 
-            csv_brand_file = os.path.join(csv_directory, f'{car_brand}.csv')
-            with open(csv_brand_file, 'w', newline='', encoding='utf-8') as brand_file:
-                writer_brand = csv.writer(brand_file)
-                writer_brand.writerow(
-                    ["Car_ID", "ID", "Brand", "Model", "Mileage", "Price [PLN]", "Price [EUR]", "Engine_Power",
-                     "GearBox", "Year", "Fuel_Type", "Horse_Power", "On_Page"])
-                for data_row in brand_data:
-                    writer_brand.writerow(data_row)
+                    if current_page % 50   == 0:  # Jeśli liczba strony jest wielokrotnością 10
+                        # Zapisz dane do pliku CSV dla danej marki
+                        with open(brand_csv_file, 'a', newline='', encoding='utf-8') as brand_file:
+                            writer_brand = csv.writer(brand_file)
+                            if current_page == 10:  # Zapisz nagłówki tylko raz na początku
+                                writer_brand.writerow(
+                                    ["Car_ID", "ID", "Brand", "Model", "Mileage", "Price [PLN]", "Price [EUR]", "Engine_Power",
+                                     "GearBox", "Year", "Fuel_Type", "Horse_Power", "On_Page"])
+                            for data_row in brand_data:
+                                writer_brand.writerow(data_row)
 
-                print(f"Dane dla marki {car_brand} zostały zapisane do pliku {csv_brand_file}")
+                            print(f"Dane dla marki {car_brand} zostały zapisane do pliku {brand_csv_file}")
 
-            # Dodaj dane z plików marki do pliku data.csv
-        for file_name in os.listdir(csv_directory):
-            if file_name.endswith(".csv"):
-                csv_brand_file = os.path.join(csv_directory, file_name)
-                # Pomijanie pliku data.csv
-                if csv_brand_file != csv_data_file:
-                    with open(csv_brand_file, 'r', newline='', encoding='utf-8') as brand_csv:
+                        # Wyczyść dane dla danej marki, aby nie powielać ich w pliku data.csv
+                        brand_data = []
+
+            # Jeśli istnieje plik CSV dla danej marki, dodaj dane do pliku danych
+            if os.path.exists(brand_csv_file):
+                with open(csv_data_file, 'a', newline='', encoding='utf-8') as data_file:
+                    writer_data = csv.writer(data_file)
+                    with open(brand_csv_file, 'r', newline='', encoding='utf-8') as brand_csv:
                         reader_brand = csv.reader(brand_csv)
-                        # Pominięcie pierwszego wiersza (nagłówków)
-                        next(reader_brand)
-                        # Dodanie danych z pliku marki do data.csv
+                        next(reader_brand)  # Skip header
                         for row in reader_brand:
                             writer_data.writerow(row)
 
 except Exception as e:
     print(f"Error: {e}")
+
 finally:
     driver.quit()
