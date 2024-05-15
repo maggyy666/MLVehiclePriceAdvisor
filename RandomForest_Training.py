56b268a26bad6be2336057d9a662365a011156a9
maggyy666 <kczyz20@gmail.com>
2024-05-15 16:19:07 +0200

    Chose different approach

--- RandomForest_Training.py
+++ RandomForest_Training.py
@@ -1,32 +1,50 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestRegressor
-from joblib import dump
+import joblib
+
+# Wczytaj zapisany model
+model = joblib.load('random_forest_model.pkl')
+
+# Wczytaj dane z pliku CSV
+data = pd.read_csv('cleaned_mileage_data.csv')
+data['price_eur_formatted'] = data['price_eur_formatted'].str.replace(',', '').astype(float)
+
+# Kodowanie zmiennych kategorycznych za pomocą get_dummies
+data_encoded = pd.get_dummies(data, columns=['brand_name', 'car_model', 'gearbox', 'fuel'])
 
 
-data = pd.read_csv("cleaned_mileage_data.csv")
+# Funkcja do przewidywania ceny na podstawie marki i modelu pojazdu
+def predict_price(brand_name, car_model):
+    if (data_encoded['brand_name_' + brand_name] == 1).any() & (data_encoded['car_model_' + car_model] == 1).any():
+        # Znajdź indeks rekordu dla podanej marki i modelu
+        idx = (data_encoded['brand_name_' + brand_name] == 1) & (data_encoded['car_model_' + car_model] == 1)
+        # Przewidź cenę dla danego indeksu
+        predicted_price = model.predict(data_encoded[idx].drop(['price_eur_formatted'], axis=1))
+        return predicted_price[0]
+    else:
+        return "Nie znaleziono informacji dla podanej marki i modelu."
 
-# Fill missing values in the 'mileage' column with zero
-data['mileage'].fillna('0', inplace=True)
 
-# Wykonywanie kodowania kategorycznego na kolumnach "brand_name" i "car_model"
-data = pd.get_dummies(data, columns=['brand_name', 'car_model'])
+def display_models_by_brand(brand_name):
+    models = data.loc[data['brand_name'] == brand_name, 'car_model'].unique()
+    print("Modele marki", brand_name + ":")
+    for i, model in enumerate(models, 1):
+        if i % 5 == 0:
+            print(model)
+        else:
+            print(model, end=", ")
+    print()
 
-# Konwersja kolumn "mileage" i "price_pln_formatted" na float
-data['mileage'] = data['mileage'].astype(str).str.replace(',', '').astype(float)
-data['price_pln_formatted'] = data['price_pln_formatted'].str.replace(',', '').astype(float)
 
-# Usunięcie niepotrzebnych kolumn
-data.drop(columns=['price_eur_formatted'], inplace=True)
+# Przykład użycia programu
+if __name__ == "__main__":
+    brand_name = input("Podaj markę pojazdu: ")
 
-X = data.drop(columns=['price_pln_formatted'])
-y = data['price_pln_formatted']
+    # Wyświetl wszystkie modele danej marki
+    display_models_by_brand(brand_name)
 
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-scores = cross_val_score(model, X, y, cv=5, n_jobs=-1)
-
-print("Cross-Validation scores:", scores)
-print("Mean R^2:", scores.mean())
-
-dump(model, 'random_forest_model.pkl')
+    car_model = input("Podaj model pojazdu: ")
+    predicted_price = predict_price(brand_name, car_model)
+    if isinstance(predicted_price, str):
+        print(predicted_price)
+    else:
+        print(f"Optymalna cena dla {brand_name} {car_model}: {predicted_price} EUR")
